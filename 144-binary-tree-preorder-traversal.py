e430dc95589745022e2dbdc253193004a72ba7fd
Peter Lin <peterlin741@gmail.com>
2018-11-16 01:10:57 -0500

    144 binary tree preorder traversal

---
+++ 144-binary-tree-preorder-traversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        def helper(root, sol):
+            if root:
+                sol.append(root.val)
+                if root.left:
+                    helper(root.left, sol)
+                if root.right:
+                    helper(root.right, sol)                
+        
+        sol = []
+        helper(root, sol)
+        return sol
