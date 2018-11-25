06467c28cd4142bc7f8bf4672c501473f5960468
Peter Lin <peterlin741@gmail.com>
2018-11-24 21:39:48 -0500

    743 network delay time

---
+++ 743-network-delay-time.py
@@ -0,0 +1,31 @@
+class Solution:
+    def networkDelayTime(self, times, N, K):
+        """
+        :type times: List[List[int]]
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        graph = {}
+        for u, v, weight in times:
+            if u not in graph:
+                graph[u] = []
+            if v not in graph:
+                graph[v] = []
+            graph[u].append((v, weight))
+        
+        dist = {}
+        heap = [(0,K)]
+        while heap:
+            d, node = heapq.heappop(heap)
+            if node in dist:
+                continue
+            dist[node] = d
+            for v, weight in graph[node]:
+                if v not in dist:
+                    heapq.heappush(heap, (d + weight, v))
+        
+        if len(dist) == N:
+            return max(dist.values())
+        else:
+            return -1
