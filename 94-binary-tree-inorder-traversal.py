02c5892d971f6cd13870103dbfb9f32f5145ede2
Peter Lin <peterlin741@gmail.com>
2018-11-28 22:47:59 -0500

    94 binary in order iterative

--- 94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal.py
@@ -21,3 +21,22 @@ class Solution:
         sol = []
         helper(root, sol)
         return sol
+
+    def inorderTraversalIterative(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        sol = []
+        stack = []
+        curr = root
+        
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            curr = stack.pop()
+            sol.append(curr.val)
+            curr = curr.right
+        
+        return sol
