b04a20c569a2d431347691385b865b64f1806522
Peter Lin <peterlin@Amandas-MBP.fios-router.home>
2018-12-10 00:06:36 -0500

    Redo 743 network delay time

--- 743-network-delay-time.py
+++ 743-network-delay-time.py
@@ -8,26 +8,29 @@ class Solution:
         :type K: int
         :rtype: int
         """
+        # Dijkstra's is like breadth first search with heaps
+        
+        # Construct the graph, with lists of neighbors
         graph = {}
         for u, v, weight in times:
-            if u not in graph:
+            if u not in graph: # Ensure all nodes included
                 graph[u] = []
             if v not in graph:
                 graph[v] = []
             graph[u].append((v, weight))
         
-        dist = {}
-        heap = [(0,K)] # starting at node K
+        dist = {} # keep track of all times from k
+        heap = [(0, K)] # starting at node K
         while heap:
-            d, node = heapq.heappop(heap)
-            if node in dist:
-                continue
-            dist[node] = d
-            for v, weight in graph[node]:
-                if v not in dist:
-                    heapq.heappush(heap, (d + weight, v))
+            d, node = heapq.heappop(heap) # heap tuples are switched
+            if node not in dist:
+                dist[node] = d
+                for v, weight in graph[node]:
+                    if v not in dist:
+                        heapq.heappush(heap, (weight + d, v))
         
-        if len(dist) == N: # ensure there are N total nodes covered
+        # Check if all N nodes visited in dist
+        if len(dist) == N:
             return max(dist.values())
         else:
             return -1
