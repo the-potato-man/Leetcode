0cf39db99c027ec2f19339ffd97148718c94f7ef
Peter Lin <peterlin741@gmail.com>
2018-12-14 14:00:09 -0500

    Updating notes

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-'''
-To Read
+# For Testing
 
-https://github.com/kradio3/google-interview-university
-
-http://juliachencoding.blogspot.com/2016/04/skip-list.html
-
-https://www.geeksforgeeks.org/amazon-interview-preparation/
-https://www.geeksforgeeks.org/amazon-interview-experience-set-262-for-sde1/
-
-Some leetcode questions to work on first:
-1. reverse half linked list
-2. priority queue
-3. number of island
-4. flying ticket
-5. unsorted slots with numbers
-6. Sum query 2nd mutable
-7. word search in matrix
-8. course schedule
-
-'''
