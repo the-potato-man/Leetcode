a721564c841545fa367005065015ee87db1660da
Peter Lin <peterlin741@gmail.com>
2018-12-14 15:40:15 -0500

    652 find duplicate subtrees

---
+++ 652-find-duplicate-subtrees.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[TreeNode]
+        """
+        def helper(root, count, res):
+            if not root:
+                return '#'
+            val = str(root.val)
+            serial = val + ',' + helper(root.left, count, res) + ',' + helper(root.right, count, res)
+            if serial in count:
+                count[serial] += 1
+            else:
+                count[serial] = 1
+            if count[serial] == 2:
+                res.append(root)
+            return serial
+        
+        count = {}
+        res = []
+        helper(root, count, res)
+        return res
