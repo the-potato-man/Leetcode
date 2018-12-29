4829557a52416a206d9666302450f4c0a0149472
Peter Lin <peterlin741@gmail.com>
2018-12-29 02:01:33 -0500

    340 updating code, still inefficient

--- others/340-longest-substring-most-k-distinct.py
+++ others/340-longest-substring-most-k-distinct.py
@@ -1,5 +1,4 @@
 # This solution is inefficient according to leetcode
-# Probably due to isValid() which traverses through the entire count array
 
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s, k):
@@ -8,29 +7,28 @@ class Solution:
         :type k: int
         :rtype: int
         """
-        def isValid(count, k):
-            numUnique = 0
-            for i in range(len(count)):
-                if count[i] > 0:
-                    numUnique += 1
-            return k >= numUnique
-
         if k == 0: return 0
         n = len(s)
         if n == 0: return 0
         if n == 1: return 1 if k > 0 else 0
         
-        count = [0] * 128
+        count = {}
         left = 0
         right = 0
         maxWindowSize = 0
         
-        count[ord(s[0])-ord('a')] += 1
+        count[s[0]] = 1
         for i in range(1,n):
-            count[ord(s[i])-ord('a')] += 1
+            if s[i] in count:
+                count[s[i]] += 1
+            else:
+                count[s[i]] = 1
             right += 1
-            while not isValid(count, k) and left < right:
-                count[ord(s[left])-ord('a')] -= 1
+            while len(count) > k and left < right:
+                if count[s[left]] == 1:
+                    del count[s[left]]
+                else:
+                	count[s[left]] -= 1
                 left += 1
             windowSize = right - left + 1
             maxWindowSize = max(maxWindowSize, windowSize) 
