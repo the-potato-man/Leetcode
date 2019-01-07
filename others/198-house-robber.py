434e794caadb1687964d5bd90ec3d2176777f010
Peter Lin <peterlin741@gmail.com>
2019-01-06 23:01:59 -0500

    Adding dp memo sol to 198 house robber

--- others/198-house-robber.py
+++ others/198-house-robber.py
@@ -4,6 +4,21 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
+        if not nums: return 0
+        
+        memo = [0] * (len(nums) + 1)
+        memo[1] = nums[0]
+        
+        for i in range(1, len(nums)):
+            memo[i+1] = max(memo[i], memo[i-1] + nums[i]) # nums is 1 pos behind memo
+        
+        return memo[-1]
+
+    def rob2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         prevMax = 0
         currMax = 0
         
