2b89ab2c6873643dee85aac82de9c215e54a4d98
Peter Lin <peterlin741@gmail.com>
2019-01-13 21:42:21 -0500

    Update 53

--- others2/53-max-subarray.py
+++ others2/53-max-subarray.py
@@ -12,3 +12,35 @@ class Solution:
             sol = max(sol, tempMax) 
                 
         return sol
+
+    def maxSubArrayDivideAndConquer(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        def maxCrossingSum(arr, l, m, r): 
+            # Left of mid
+            curr = 0
+            leftSum = float('-inf')
+            for i in range(m, l-1, -1): 
+                curr += arr[i] 
+                if curr > leftSum: leftSum = curr 
+
+            # Right of mid 
+            curr = 0
+            rightSum = float('-inf')
+            for i in range(m+1, r+1): 
+                curr += arr[i] 
+                if curr > rightSum: rightSum = curr 
+                    
+            return leftSum + rightSum; 
+
+        def maxSubArraySum(arr, l, r): 
+            if (l == r) : return arr[l] 
+            m = (l + r) // 2
+            return max(maxSubArraySum(arr, l, m), 
+                       maxSubArraySum(arr, m+1, r), 
+                       maxCrossingSum(arr, l, m, r))
+
+        return maxSubArraySum(nums, 0, len(nums)-1)
