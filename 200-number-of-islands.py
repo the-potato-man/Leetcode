08a11f33c580726d74d786c0941934c236692d1e
Peter Lin <peterlin741@gmail.com>
2019-01-18 01:07:45 -0500

    200 islands with disjoint set unions

--- 200-number-of-islands.py
+++ 200-number-of-islands.py
@@ -83,3 +83,64 @@ class Solution(object):
                     dfs(grid, r, c)
                         
         return numIslands
+
+    class DSU:
+        def __init__(self, grid):
+            self.count = 0
+            m = len(grid)
+            n = len(grid[0])
+            self.parent = [-1] * (m * n + 1)
+            self.rank = [0] * (m * n + 1)
+            for i in range(m):
+                for j in range(n):
+                    if grid[i][j] == '1':
+                        self.parent[i * n + j] = i * n + j
+                        self.count += 1
+
+        def find(self, x):
+            # Path Compression
+            if self.parent[x] != x:
+                self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+
+        def union(self, x, y):
+            # Union by rank
+            xr = self.find(x)
+            yr = self.find(y)
+
+            if xr != yr:
+                if self.rank[xr] < self.rank[yr]:
+                    self.parent[yr] = xr
+                elif self.rank[xr] > self.rank[yr]:
+                    self.parent[xr] = yr
+                else:
+                    self.parent[yr] = xr
+                    self.rank[yr] += 1
+                self.count -= 1
+            return True
+
+    class Solution:
+        def numIslandsUsingDisjointSet(self, grid):
+            """
+            :type grid: List[List[str]]
+            :rtype: int
+            """
+            if not grid: return 0
+            m = len(grid)
+            n = len(grid[0])
+            
+            def isValid(grid, row, col):
+                if row < 0 or col < 0 or row >= m or col >= n or grid[row][col] == '0':
+                    return False
+                return True
+
+            dsu = DSU(grid)
+            for r in range(m):
+                for c in range(n):
+                    if grid[r][c] == '1':
+                        grid[r][c] = '0'
+                        for rd, cd in [(1,0), (-1,0), (0,1), (0,-1)]:
+                            r2, c2 = r + rd, c + cd
+                            if isValid(grid, r2, c2):
+                                dsu.union(r * n + c, r2 * n + c2)
+            return dsu.count
