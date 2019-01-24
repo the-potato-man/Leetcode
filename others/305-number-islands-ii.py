630769bccefaf2162c85880cfbd17d1cfc67ea18
Peter Lin <peterlin741@gmail.com>
2019-01-24 00:12:22 -0500

    Update 305

--- others/305-number-islands-ii.py
+++ others/305-number-islands-ii.py
@@ -10,33 +10,33 @@ class Solution(object):
             return r >= 0 and c >= 0 and r < m and c < n
         
         sol = []
-        land2id = {} # position to island id
+        pos2island = {} # position to island id
         numIslands = 0
         islandId = 0
         
         for r, c in positions:
-            overlap = set()
+            neigh = set()
             for dr, dc in [(1,0), (-1,0), (0,1), (0,-1)]:
                 nextR = r + dr
                 nextC = c + dc
                 pos = nextR * n + nextC
-                if isValid(nextR, nextC) and pos in land2id:
-                    overlap.add(land2id[pos])
+                if isValid(nextR, nextC) and pos in pos2island:
+                    neigh.add(pos2island[pos])
             
             key = r * n + c
-            if not overlap:
+            if not neigh:
                 numIslands += 1
                 islandId += 1                
-                land2id[key] = islandId
-            elif len(overlap) == 1:
-                land2id[key] = next(iter(overlap))
+                pos2island[key] = islandId
+            elif len(neigh) == 1:
+                pos2island[key] = next(iter(neigh))
             else:
-                rootId = next(iter(overlap))
-                for pos, landId in land2id.items():
-                    if landId in overlap:
-                        land2id[pos] = rootId
-                land2id[key] = rootId
-                numIslands -= (len(overlap) - 1)
+                rootId = next(iter(neigh))
+                for pos, landId in pos2island.items():
+                    if landId in neigh:
+                        pos2island[pos] = rootId
+                pos2island[key] = rootId
+                numIslands -= (len(neigh) - 1)
             sol.append(numIslands)
             
         return sol
