1844ea59c0072bb20b8fc90c2961bee28f3016c3
Peter Lin <peterlin741@gmail.com>
2019-01-28 23:57:10 -0500

    notes

--- others/753-cracking-the-safe.py
+++ others/753-cracking-the-safe.py
@@ -1,3 +1,13 @@
+'''
+There is a box protected by a password. The password is n digits, where each letter can be one of the first k digits 0, 1, ..., k-1.
+
+You can keep inputting the password, the password will automatically be matched against the last n digits entered.
+
+For example, assuming the password is "345", I can open it when I type "012345", but I enter a total of 6 digits.
+
+Please return any string of minimum length that is guaranteed to open the box after the entire string is inputted.
+'''
+
 class Solution(object):
     def crackSafe(self, n, k):
         """
