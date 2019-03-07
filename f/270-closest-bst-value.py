7ac44bb0ef7500a2c22a2b16b8309765c03fbe82
Peter Lin <peterlin@Amandas-MBP.fios-router.home>
2019-03-06 22:56:06 -0500

    270 closest bst value

---
+++ f/270-closest-bst-value.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def closestValue(self, root: TreeNode, target: float) -> int:
+        if not root: return None
+        cand = root.val
+        while root:
+            if root.val == target: return root.val
+            if abs(root.val - target) < abs(cand - target):
+                cand = root.val
+            if root.val > target:
+                root = root.left
+            else:
+                root = root.right
+        return cand
