b95120fcd457ee5fe572695f39da2f7f0416591b
Peter Lin <peterlin741@gmail.com>
2019-03-21 16:11:55 -0400

    nmotes 298

--- f/298-binary-tree-longest-consecutive.py
+++ f/298-binary-tree-longest-consecutive.py
@@ -5,6 +5,27 @@ class TreeNode:
         self.left = None
         self.right = None
 
+'''
+Given a binary tree, find the length of the longest consecutive sequence path.
+
+The path refers to any sequence of nodes from some starting node to any node in the tree along the parent-child connections. 
+The longest consecutive path need to be from parent to child (cannot be the reverse).
+
+Input:
+
+   1
+    \
+     3
+    / \
+   2   4
+        \
+         5
+
+Output: 3
+
+Explanation: Longest consecutive sequence path is 3-4-5, so return 3.
+
+'''
 class Solution:
     def longestConsecutive(self, root: TreeNode) -> int:
         self.maxLength = 0
