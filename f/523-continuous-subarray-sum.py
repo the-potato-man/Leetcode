db1b91931442fd28d42f806ee107676bb91af8d7
Peter Lin <peterlin@Amandas-MBP.fios-router.home>
2019-04-10 21:57:47 -0400

    update comment 523

--- f/523-continuous-subarray-sum.py
+++ f/523-continuous-subarray-sum.py
@@ -1,5 +1,7 @@
 '''
-Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
+Given a list of non-negative numbers and a target integer k, 
+write a function to check if the array has a continuous subarray of size at least 2 
+that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
 '''
 class Solution(object):
     def checkSubarraySum(self, nums, k):
