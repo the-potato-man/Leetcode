93501c49162c48f329cebeb3fd8e317ee85b53ae
Peter Lin <peterlin741@gmail.com>
2019-04-14 17:02:38 -0400

    fixing comment

--- f/270-closest-bst-value.py
+++ f/270-closest-bst-value.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     # Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
