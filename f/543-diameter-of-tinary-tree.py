660d49d4bca3a26846df73c2401785519cce7f2c
Peter Lin <peterlin741@gmail.com>
2019-04-14 12:39:30 -0400

    fixing comment

--- f/543-diameter-of-tinary-tree.py
+++ f/543-diameter-of-tinary-tree.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 '''
 Given a binary tree, you need to compute the length of the diameter of the tree. 
