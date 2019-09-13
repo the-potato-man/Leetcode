c172f8819f0b5dbd265cc379bdfd3acf4a450277
Peter Lin <peterlin741@gmail.com>
2019-09-13 08:23:02 -0400

    Updating 340 longest substring most k distinct

--- 2019/go/340-longest-substring-most-k-distinct.py
+++ 2019/go/340-longest-substring-most-k-distinct.py
@@ -9,36 +9,33 @@ Output: 3
 Explanation: T is "ece" which its length is 3.
 '''
 class Solution:
-    def lengthOfLongestSubstringKDistinct(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        if k == 0: return 0
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        
+        if k == 0: return  0
         n = len(s)
         if n == 0: return 0
         if n == 1: return 1 if k > 0 else 0
         
-        count = {}
-        left = 0
-        right = 0
+        window = {}
+        l, r = 0, 0
         maxWindowSize = 0
         
-        count[s[0]] = 1
-        for i in range(1,n):
-            if s[i] in count:
-                count[s[i]] += 1
+        while r < len(s):
+            if s[r] in window:
+                window[s[r]] += 1
             else:
-                count[s[i]] = 1
-            right += 1
-            while len(count) > k and left < right:
-                if count[s[left]] == 1:
-                    del count[s[left]]
+                window[s[r]] = 1
+            
+            while l < r and k < len(window):
+                if window[s[l]] == 1:
+                    del window[s[l]]
                 else:
-                	count[s[left]] -= 1
-                left += 1
-            windowSize = right - left + 1
+                	window[s[l]] -= 1
+                l += 1
+        
+            windowSize = r - l + 1
             maxWindowSize = max(maxWindowSize, windowSize) 
+            
+            r += 1
         
         return maxWindowSize
